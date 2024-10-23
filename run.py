829c4b3eef2e526e8b5876a2b7cd4128a9c1ef50
sajkcaz19 <canatea@utb.edu.co>
2024-10-23 07:47:04 -0500

    Docker-compose y Dockerfile

--- run.py
+++ run.py
@@ -1,5 +1,32 @@
+from flask import Flask, jsonify, request
 from flaskDirectory import create_app
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    new_task = request.json
+    tasks.append(new_task)
+    return jsonify(new_task), 201
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    updated_task = request.json
+    tasks[task_id] = updated_task
+    return jsonify(updated_task)
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    tasks.pop(task_id)
+    return '', 204
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=8000)
+    
